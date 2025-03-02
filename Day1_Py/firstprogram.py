71e5f74067fd07eb9fb498920785ed3ee34bda6a
bodoz <118077974+zainea-bogdan@users.noreply.github.com>
2025-03-02 12:37:28 +0200

    Tested a few functionalities
    i tested a few things to get used to python workflow and logic behind the code line and i also imported numpy module

--- Day1_Py/firstprogram.py
+++ Day1_Py/firstprogram.py
@@ -1,1 +1,25 @@
-print("hello world")
+# print("hello world")
+# if(3<2):
+#     print("3 greater than 2")
+# else:
+#     print("2 is greater than 3")
+# cos_cumparaturi =["mere","pere","alune"]
+# for i in cos_cumparaturi:
+#     print("am cumparat " + i +"\n")
+
+
+# cos_cumparaturi2 =[1,2,3]
+# for i in cos_cumparaturi2:
+#     print("am cumparat " + str(i) +"\n") #it is required to make explicit conversion when concatenating, if not then you got run time error
+
+# pentru a face inline comment i use # and if i want block of comment then i use triple " sau ' sau combinatii de astea doua 
+# import math 
+# import numpy as np
+
+# print(int(math.sqrt(16)))
+# array = np.random.randn(5)
+# print("valori random: ",array)
+
+#!!!! regarding the venv, they are recommended to be created when wokring on different projects, because if for example a prj use numpy older version and other prjt use a newer version, that it causes conflicts 
+
+
